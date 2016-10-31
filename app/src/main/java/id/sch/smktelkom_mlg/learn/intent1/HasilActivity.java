e3b2b62563a669ae2fc90d2bce083be50bf67365
cintaberliana <cintaberlianaagg@gmail.com>
2016-10-31 22:01:57 +0700

    onBackPressed Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HasilActivity extends AppCompatActivity {
@@ -21,11 +22,17 @@ public class HasilActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       if (item.getItemId()==android.R.id.home)
+       {
+           onBackPressed();
+           return true;
+       }
+        return super.onOptionsItemSelected(item);
     }
 }
